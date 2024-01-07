fa512d440cad0522289889e6479805c3a608ac9b
phamquan28 <quanphph28222>
2024-01-08 00:41:24 +0700

    project_DATN

---
+++ src/main/java/com/example/datn_nicestore/DatnNiceStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.datn_nicestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatnNiceStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatnNiceStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/datn_nicestore/DatnNiceStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datn_nicestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatnNiceStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
